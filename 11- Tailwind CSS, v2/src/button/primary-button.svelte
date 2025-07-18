8aba5c9ced531c332c44c13e9ede63ee8322a9e7
Karim <amanbyword@gmail.com>
2025-07-18 16:28:44 +0300

    11- Tailwind CSS, v2

---
+++ 11- Tailwind CSS, v2/src/button/primary-button.svelte
@@ -0,0 +1,6 @@
+<script>
+  const { label = ' Primary Button', ...props } = $props();
+</script>
+
+<button class="bg-blue-600 px-3 py-1.5 rounded-md text-white 
+shadow-md shadow-gray-500 hover:bg-blue-200">{label}</button>
---
+++ 11- Tailwind CSS, v2/src/button/secondary-button.svelte
@@ -0,0 +1,5 @@
+<script>
+  const { label = 'Seconadary Button', ...props } = $props();
+</script>
+<button class="rounded-md p-2 text-black
+shadow-md shadow-gray-300 outline-teal-400 focus:outline-2  hover:outline-red-200">{label}</button>
